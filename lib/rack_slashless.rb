b998fdb955b0dbb2da1e03439954140f38c808fe
Kevin Thompson <kevin@kevinthompson.info>
2012-12-15 12:20:56 -0800

    Reformat indentation.

--- lib/rack_slashless.rb
+++ lib/rack_slashless.rb
@@ -9,12 +9,11 @@ module Rack
       request = Rack::Request.new(env)
       if request.get? && request.path_info.match(/\w+\/$/)
         destination = [
-                        "#{request.scheme}://",
-                        request.env['SERVER_NAME'],
-                        request.path_info[0..-2],
-                        (request.query_string.empty? ? '' : "?#{request.query_string}")
-                      ].join
-
+          "#{request.scheme}://",
+          request.env['SERVER_NAME'],
+          request.path_info[0..-2],
+          (request.query_string.empty? ? '' : "?#{request.query_string}")
+        ].join
         [301, {'Location' => destination}, ['Redirecting to the same url but with the ending /']]
       else
         @app.call(env)
