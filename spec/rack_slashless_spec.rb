71bf06389a44fcee82a91fc14d54019f2a2f4794
Kevin Thompson <kevin@kevinthompson.info>
2012-12-15 15:51:51 -0800

    Add tests for urls with subdomains and ports, and without trailing slashes.

--- spec/rack_slashless_spec.rb
+++ spec/rack_slashless_spec.rb
@@ -17,6 +17,13 @@ describe Rack::Slashless do
       end
     end
 
+    context 'with a path that does not include a trailing slash' do
+      it 'should not redirect' do
+        get '/blog', {}, 'HTTP_HOST' => 'example.org'
+        last_response.status.should == 200
+      end
+    end
+
     context 'with a path that includes a trailing slash' do
       it 'should redirect to a url without a trailing slash' do
         get '/blog/', {}, 'HTTP_HOST' => 'example.org'
@@ -30,6 +37,22 @@ describe Rack::Slashless do
         get '/blog/?article=1', {}, 'HTTP_HOST' => 'example.org'
         last_response.status.should == 301
         last_response['Location'].should eql('http://example.org/blog?article=1')
+      end
+    end
+
+    context 'with a port' do
+      it 'should retain the port when redirecting' do
+        get '/blog/', {}, 'HTTP_HOST' => 'example.org', 'SERVER_PORT' => '3000'
+        last_response.status.should == 301
+        last_response['Location'].should eql('http://example.org:3000/blog')
+      end
+    end
+
+    context 'with a subdomain' do
+      it 'should retain the subdomain when redirecting' do
+        get '/blog/', {}, 'HTTP_HOST' => 'www.example.org'
+        last_response.status.should == 301
+        last_response['Location'].should eql('http://www.example.org/blog')
       end
     end
 
