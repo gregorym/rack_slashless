ffa806d1b3fac72caec5634c8f418bcfff2aca82
Kevin Thompson <kevin@kevinthompson.info>
2012-12-15 12:22:33 -0800

    Add destination URL to redirect message.

--- lib/rack_slashless.rb
+++ lib/rack_slashless.rb
@@ -14,7 +14,7 @@ module Rack
           request.path_info[0..-2],
           (request.query_string.empty? ? '' : "?#{request.query_string}")
         ].join
-        [301, {'Location' => destination}, ['Redirecting to the same url but with the ending /']]
+        [301, {'Location' => destination}, ["Redirecting to #{destination}"]]
       else
         @app.call(env)
       end
